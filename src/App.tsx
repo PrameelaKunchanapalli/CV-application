3aa33f624f763af16aaef255d6d162bb9e918f35
Satya Prameela Devi Kunchanapalli <idisprameela@gmail.com>
2025-05-09 14:44:50 +0530

    error in css

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import GeneralInfo from './components/GeneralInfo';
-import Education from './components/Education';
-import Experience from './components/Experience';
-import './styles/shared.css';
-
-
+import React from "react";
+import GeneralInfo from "./components/GeneralInfo";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import "./styles/shared.css";
 
 const App: React.FC = () => {
-//function App() {
+  //function App() {
   return (
     <div className="app-wrapper">
       <h1>My CV</h1>
-      <GeneralInfo/>
-      <Education/>
-      <Experience/>
-
+      <GeneralInfo />
+      <Education />
+      <Experience />
     </div>
   );
 };
